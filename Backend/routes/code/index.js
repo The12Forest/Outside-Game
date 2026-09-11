663683c76590e50aa35dab5db96fb34fec0bf4cb
The12Forest <manuel@steka.ch>
2026-09-11 07:42:44 +0200

    temp

--- Backend/routes/code/index.js
+++ Backend/routes/code/index.js
@@ -4,7 +4,18 @@ import path from "path";
 import log from '../../functions/log.js';
 const console = { log: log('CodeRouter') };
 const router = express.Router()
-let active_code = [123]
+let active_code = [123, 456]
+//first code is runner seccond code is catcher
+
+function makeid(length) {
+    var result = '';
+    var characters = '0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 router.get("/check", async (req, res) => {
     let code = req.query.code ?? req.params.code ?? req.body.code
@@ -15,11 +26,39 @@ router.get("/check", async (req, res) => {
             secure: true,  
             sameSite: 'lax'
         });
-        res.status(200).json({ ok: true, Reason: "Code Correct!" })
+        if (codeID % 2) {
+            res.status(200).json({ ok: true, Group: "runner", Reason: "Code Correct!" })
+        } else {
+            res.status(200).json({ ok: true, Group: "catcher", Reason: "Code Correct!" })
+        }
     } else {
         res.status(401).json({ ok: false, Reason: "Code Wrong!" })
     }
 })
+
+router.get("/newteam", async (req, res) => {
+    let code_runner  = makeid(4)
+    let code_catcher = makeid(4)
+    while (code_catcher == code_runner) {
+        let code_catcher = makeid(4)
+    }
+    active_code.push(code_runner)
+    active_code.push(code_catcher)
+
+    res.cookie('code_runner', code_runner, {
+        maxAge: 240000,
+        secure: true,
+        sameSite: 'lax'
+    });
+    res.cookie('code_catcher', code_catcher, {
+        maxAge: 240000,
+        secure: true,
+        sameSite: 'lax'
+    });
+
+    res.status(200).json({ ok: true, runner: code_runner, catcher: code_catcher })
+})
+
 
 
 export { router }
